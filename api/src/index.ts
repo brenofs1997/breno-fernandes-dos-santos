14c2afbbfe2ac034bda555a61ecc410d21636e86
PauloFH99 <brenofs1997@gmail.com>
2023-01-11 14:40:58 -0300

    feat:create connection mongodb on docker

---
+++ api/src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { router } from './router';
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+
+    const app = express();
+    const port = 3001;
+
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      next();
+    });
+    
+    app.use(express.json());
+    app.use(router);
+
+    app.listen(3001, () => {
+      console.log(`🚀  Server is Running on http://localhost:${port}`);
+    });
+  })
+  .catch (()=> console.log('Erro ao conectar ao mongodb') );
+
+
+
